58c88e3c6ea0e551355aa2beaf57e56b71fa0cbd
Vishwanatha Subbanna <vishwanatha.subbanna@metaco.com>
2024-10-29 19:32:31 +0530

    Fix name of code engine project
    Signed-off-by: Vishwanatha Subbanna <vishwanatha.subbanna@metaco.com>

--- code-engine-app.tf
+++ code-engine-app.tf
@@ -3,9 +3,18 @@ locals {
   tags = ["ibmcloud", "codeengine", "code_engine_app", var.prefix]
 }
 
+# Generate a random string. This is needed because the code engine project does not get deleted immediately on destroy
+resource "random_string" "lowercase" {
+  length = 8   # Length of the generated string
+  lower  = true  # Only include lowercase letters
+  upper  = false  # Exclude uppercase letters
+  number = false  # Exclude numbers
+  special = false  # Exclude special characters
+}
+
 # Each code engine needs to be under a project
 resource "ibm_code_engine_project" "code_engine_app_project" {
-  name              = format("%s-code-engine-app-project", var.prefix)
+  name              = format("%s-%s-code-engine-app-project", var.prefix, random_string.lowercase.result)
   resource_group_id = data.ibm_resource_group.resource_group.id
 }
 
